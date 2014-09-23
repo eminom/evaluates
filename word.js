12293cfa4e0ccc569c950e1facac4a0f15874a27
baihai <hai.bai.cn@gmail.com>
2014-09-23 20:55:43 +0800

    yes

--- word.js
+++ word.js
@@ -2,24 +2,30 @@
 
 var libxmljs = require('libxmljs');
 var fs = require('fs');
-var u = require('./parse_comm');
+var ut = require('./parse_comm');
 
-var data = fs.readFileSync('word.xml').toString('utf8');
+var data = fs.readFileSync('designs/xmls/word.xml').toString('utf8');
 var xmlDoc = libxmljs.parseXml(data);
 
 function main(){
 	//OK, this is the roots
+
+	ut.print('-- This file is generated automatically');
+	ut.print('-- Do not modify this file manually');
+	ut.print('WordDictionary = ReadOnly { ');
 	var heroes = xmlDoc.get('//wordInfo');
 	var kids = heroes.childNodes();
 	for(var i=0;i<kids.length;++i){
 		if(kids[i].name() != 'text') {
-			var id = u.getField(kids[i],'wordId');
+			var id = ut.getField(kids[i],'wordId');
 			var msg = '[\'' + id + '\']' + ' = {';
-			u.print(msg, 1);
-			u.traverseNode(kids[i], 2);
-			u.print('},  ', 1);
+			ut.print(msg, 1);
+			ut.traverseNode(kids[i], 2);
+			ut.print('},  ', 1);
+			ut.print();
 		}
 	}
+	ut.print('}');
 }
 
 //Starting here.
