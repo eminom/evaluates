64b1cf30b6d5951a4d44282067b52d4ae860c2da
baihai <hai.bai.cn@gmail.com>
2014-10-30 15:59:23 +0800

    fixed

--- js/hero.js
+++ js/hero.js
@@ -5,19 +5,19 @@ var fs = require('fs');
 var ut = require('./parse_comm');
 var assert = require('assert');
 
-var data = fs.readFileSync('designs/xmls/hero.xml').toString('utf8');
+var data = fs.readFileSync('designs/xmls/heroConf.xml').toString('utf8');
 var xmlDoc = libxmljs.parseXml(data);
 
 function main(){
 	//OK, this is the roots
-	var heroes = xmlDoc.get('//heroInfo');
+	var heroes = xmlDoc.get('//heroConf');
 	var kids = heroes.childNodes();
 	ut.print('-- This file is generated automatically')
 	ut.print('-- Do not modify this file manually')
 	ut.print('HeroProperties = ReadOnly{');
 	for(var i=0;i<kids.length;++i){
 		if(kids[i].name() != 'text') {
-			var id = kids[i].attr('heroId');
+			var id = kids[i].attr('heroID');
 			assert(id != null, 'must be there');
 			var msg = '[\'' + id.value() + '\']' + ' = ReadOnly{';
 			ut.print(msg, 1);
